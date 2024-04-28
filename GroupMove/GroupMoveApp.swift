0658664984efee63b3d3553900df98c428ea35e8
Johnny Leung <johnnyleung2001@gmail.com>
2024-04-28 18:38:57 -0400

    Added property data structure

--- GroupMove/GroupMoveApp.swift
+++ GroupMove/GroupMoveApp.swift
@@ -13,7 +13,7 @@ struct GroupMoveApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            PropertyItemView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
